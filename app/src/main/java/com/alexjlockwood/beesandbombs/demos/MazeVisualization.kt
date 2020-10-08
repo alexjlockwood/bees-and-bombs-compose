309799e3576aaf583c8ad6579273b7e3558759cb
Alex Lockwood <alockwood@lyft.com>
2020-10-07 20:42:51 -0700

    delete wilsons algorithm for now

--- app/src/main/java/com/alexjlockwood/beesandbombs/demos/MazeVisualization.kt
+++ app/src/main/java/com/alexjlockwood/beesandbombs/demos/MazeVisualization.kt
@@ -34,7 +34,6 @@ enum class MazeType {
     RANDOMIZED_TRAVERSAL,
     RANDOMIZED_DEPTH_FIRST_TRAVERSAL,
     PRIMS_ALGORITHM,
-    WILSONS_ALGORITHM,
 }
 
 @Composable
@@ -45,28 +44,28 @@ fun MazeVisualization(modifier: Modifier = Modifier) {
     Box(
         modifier
             .clickable(onClick = {
+                // Tap the screen to cycle through the different maze types.
                 mazeType = mazeTypes[(mazeTypes.indexOf(mazeType) + 1) % mazeTypes.size]
                 frameCounter = 0
             })
             .drawWithCache {
-                val cellWidth = size.cellWidth()
-                val cellHeight = size.cellHeight()
+                val mazeWidth = size.mazeWidth()
+                val mazeHeight = size.mazeHeight()
                 val cells = when (mazeType) {
-                    RANDOMIZED_TRAVERSAL -> generateMaze(RandomizedTraversalHeap(), cellWidth, cellHeight)
-                    RANDOMIZED_DEPTH_FIRST_TRAVERSAL -> generateMaze(RandomizedDepthFirstHeap(), cellWidth, cellHeight)
-                    PRIMS_ALGORITHM -> generateMaze(PrimsHeap(), cellWidth, cellHeight)
-                    WILSONS_ALGORITHM -> generateWilsonsMaze(cellWidth, cellHeight)
+                    RANDOMIZED_TRAVERSAL -> generateMaze(RandomizedTraversalHeap(), mazeWidth, mazeHeight)
+                    RANDOMIZED_DEPTH_FIRST_TRAVERSAL -> generateMaze(RandomizedDepthFirstHeap(), mazeWidth, mazeHeight)
+                    PRIMS_ALGORITHM -> generateMaze(PrimsHeap(), mazeWidth, mazeHeight)
                 }
-                val fills = arrayOfNulls<Color>(cellWidth * cellHeight).toMutableList()
-//                val frontier = mutableListOf((cellWidth shr 1) + (cellHeight shr 1) * cellWidth)
-                val frontier = mutableListOf(0)//(cellWidth shr 1) + (cellHeight shr 1) * cellWidth)
+                val fills = arrayOfNulls<Color>(mazeWidth * mazeHeight).toMutableList()
+                // val frontier = mutableListOf((mazeWidth shr 1) + (mazeHeight shr 1) * mazeWidth)
+                val frontier = mutableListOf(0)
 
                 onDraw {
                     drawRect(Color.Black)
 
                     translate(
-                        round((size.width - cellWidth * CELL_SIZE - (cellWidth + 1) * CELL_SPACING) / 2f),
-                        round((size.height - cellHeight * CELL_SIZE - (cellHeight + 1) * CELL_SPACING) / 2f),
+                        round((size.width - mazeWidth * CELL_SIZE - (mazeWidth + 1) * CELL_SPACING) / 2f),
+                        round((size.height - mazeHeight * CELL_SIZE - (mazeHeight + 1) * CELL_SPACING) / 2f),
                     ) {
                         val nextFrontier = mutableListOf<Int>()
                         val currentColor = sinebow(frameCounter / COLOR_SPEED)
@@ -74,8 +73,8 @@ fun MazeVisualization(modifier: Modifier = Modifier) {
                             fills[i] = currentColor
                             if (cells[i] and E != 0 && fills[i + 1] == null) nextFrontier.add(i + 1)
                             if (cells[i] and W != 0 && fills[i - 1] == null) nextFrontier.add(i - 1)
-                            if (cells[i] and S != 0 && fills[i + cellWidth] == null) nextFrontier.add(i + cellWidth)
-                            if (cells[i] and N != 0 && fills[i - cellWidth] == null) nextFrontier.add(i - cellWidth)
+                            if (cells[i] and S != 0 && fills[i + mazeWidth] == null) nextFrontier.add(i + mazeWidth)
+                            if (cells[i] and N != 0 && fills[i - mazeWidth] == null) nextFrontier.add(i - mazeWidth)
                         }
 
                         drawMaze(cells, fills)
@@ -91,27 +90,61 @@ fun MazeVisualization(modifier: Modifier = Modifier) {
     )
 }
 
-private fun generateMaze(heap: Heap, cellWidth: Int, cellHeight: Int): List<Int> {
+private fun Size.mazeWidth() = floor((width - CELL_SPACING) / (CELL_SIZE + CELL_SPACING)).toInt()
+
+private fun Size.mazeHeight() = floor((height - CELL_SPACING) / (CELL_SIZE + CELL_SPACING)).toInt()
+
+private fun DrawScope.drawMaze(cells: List<Int>, fills: List<Color?>) {
+    for (i in cells.indices) {
+        val color = fills[i] ?: continue
+        val x = i % size.mazeWidth()
+        val y = i / size.mazeWidth()
+        val x0 = x * CELL_SIZE + (x + 1) * CELL_SPACING
+        val y0 = y * CELL_SIZE + (y + 1) * CELL_SPACING
+        drawRect(
+            color = color,
+            topLeft = Offset(x0, y0),
+            size = Size(CELL_SIZE, CELL_SIZE),
+        )
+        if (cells[i] and S != 0) {
+            drawRect(
+                color = color,
+                topLeft = Offset(x0, y0 + CELL_SPACING),
+                size = Size(CELL_SIZE, CELL_SPACING),
+            )
+        }
+        if (cells[i] and E != 0) {
+            drawRect(
+                color = color,
+                topLeft = Offset(x0 + CELL_SPACING, y0),
+                size = Size(CELL_SPACING, CELL_SIZE),
+            )
+        }
+    }
+}
+
+
+private fun generateMaze(heap: Heap, mazeWidth: Int, mazeHeight: Int): List<Int> {
     heap.add(Edge(0, N))
     heap.add(Edge(0, E))
-    val cells = (0 until cellWidth * cellHeight).map { 0 }.toMutableList()
+    val cells = (0 until mazeWidth * mazeHeight).map { 0 }.toMutableList()
     while (heap.isNotEmpty()) {
         val (i0, d0) = heap.remove()
-        val x0 = i0 % cellWidth
-        val y0 = i0 / cellWidth
+        val x0 = i0 % mazeWidth
+        val y0 = i0 / mazeWidth
         val i1: Int
         val d1: Int
         val x1: Int
         val y1: Int
         when (d0) {
             N -> {
-                i1 = i0 - cellWidth
+                i1 = i0 - mazeWidth
                 d1 = S
                 x1 = x0
                 y1 = y0 - 1
             }
             S -> {
-                i1 = i0 + cellWidth
+                i1 = i0 + mazeWidth
                 d1 = N
                 x1 = x0
                 y1 = y0 + 1
@@ -134,10 +167,10 @@ private fun generateMaze(heap: Heap, cellWidth: Int, cellHeight: Int): List<Int>
             cells[i0] = cells[i0] or d0
             cells[i1] = cells[i1] or d1
             val addedEdges = mutableListOf<Edge>()
-            if (0 < y1 && cells[i1 - cellWidth] == 0) addedEdges.add(Edge(i1, N))
-            if (y1 < cellHeight - 1 && cells[i1 + cellWidth] == 0) addedEdges.add(Edge(i1, S))
+            if (0 < y1 && cells[i1 - mazeWidth] == 0) addedEdges.add(Edge(i1, N))
+            if (y1 < mazeHeight - 1 && cells[i1 + mazeWidth] == 0) addedEdges.add(Edge(i1, S))
             if (0 < x1 && cells[i1 - 1] == 0) addedEdges.add(Edge(i1, W))
-            if (x1 < cellWidth - 1 && cells[i1 + 1] == 0) addedEdges.add(Edge(i1, E))
+            if (x1 < mazeWidth - 1 && cells[i1 + 1] == 0) addedEdges.add(Edge(i1, E))
             heap.addAll(addedEdges)
         }
     }
@@ -176,150 +209,6 @@ private class RandomizedTraversalHeap : Heap {
     override fun remove(): Edge = heap.removeAt((0 until heap.size).random())
     override fun isNotEmpty(): Boolean = heap.isNotEmpty()
     override fun addAll(edges: MutableList<Edge>) = edges.forEach { heap.add(it) }
-}
-
-private fun generateWilsonsMaze(cellWidth: Int, cellHeight: Int): List<Int> {
-    val cellRange = (0 until cellWidth * cellHeight)
-    // Each cell's edge bits.
-    val cells = cellRange.map { null }.toMutableList<Int?>()
-    // Tracks the current random walk path.
-    val prevIndexMap = cellRange.map { -1 }.toMutableList()
-    // The remaining cell indices to visit.
-    val remainingIndices = cellRange.map { it }.toMutableList()
-
-    // Add the starting cell.
-    cells[remainingIndices.removeLast()] = 0
-
-    fun eraseCurrentWalk(prevIndex: Int, currIndex: Int) {
-        var i = prevIndex
-        do {
-            val j = prevIndexMap[i]
-            prevIndexMap[i] = -1
-            i = j
-        } while (i != currIndex)
-    }
-
-    fun loopErasedRandomWalk(): Boolean {
-        var i0: Int
-        do {
-            if (remainingIndices.isEmpty()) return true
-            i0 = remainingIndices.removeLast()
-        } while (cells[i0].geq(0))
-
-        prevIndexMap[i0] = i0
-        while (true) {
-            var x0 = i0 % cellWidth
-            var y0 = i0 / cellWidth
-
-            // Pick a legal random direction at each step.
-            var i1 = (0 until 4).random()
-            if (i1 == 0) {
-                if (y0 <= 0) continue
-                --y0
-                i1 = i0 - cellWidth
-            } else if (i1 == 1) {
-                if (y0 >= cellHeight - 1) continue
-                ++y0
-                i1 = i0 + cellWidth
-            } else if (i1 == 2) {
-                if (x0 <= 0) continue
-                --x0
-                i1 = i0 - 1
-            } else {
-                if (x0 >= cellWidth - 1) continue
-                ++x0
-                i1 = i0 + 1
-            }
-
-            if (prevIndexMap[i1] >= 0) {
-                // If this new cell was visited previously during this walk,
-                // erase the loop, rewinding the path to its earlier state.
-                eraseCurrentWalk(i0, i1)
-            } else {
-                // Otherwise, just add it to the walk.
-                prevIndexMap[i1] = i0
-            }
-
-            // If this cell is part of the maze, we're done walking.
-            if (cells[i1].geq(0)) {
-                // Add the random walk to the maze by backtracking to the starting cell.
-                // Also erase this walk's history to not interfere with subsequent walks.
-                while (prevIndexMap[i1] != i1) {
-                    i0 = prevIndexMap[i1]
-                    when (i1) {
-                        i0 + 1 -> {
-                            cells[i0] = cells[i0]?.or(E) ?: E
-                            cells[i1] = cells[i1]?.or(W) ?: W
-                        }
-                        i0 - 1 -> {
-                            cells[i0] = cells[i0]?.or(W) ?: W
-                            cells[i1] = cells[i1]?.or(E) ?: E
-                        }
-                        i0 + cellWidth -> {
-                            cells[i0] = cells[i0]?.or(S) ?: S
-                            cells[i1] = cells[i1]?.or(N) ?: N
-                        }
-                        else -> {
-                            cells[i0] = cells[i0]?.or(N) ?: N
-                            cells[i1] = cells[i1]?.or(S) ?: S
-                        }
-                    }
-                    prevIndexMap[i1] = -1
-                    i1 = i0
-                }
-
-                prevIndexMap[i1] = -1
-                return false
-            }
-
-            i0 = i1
-        }
-    }
-
-
-    while (!loopErasedRandomWalk()) {
-        // Loop until there are no remaining cells, adding a loop-erased
-        // random walk to the maze each iteration.
-    }
-
-    return cells.map { it!! }
-}
-
-private fun Int?.geq(n: Int) = this != null && this >= n
-
-private fun DrawScope.cellWidth() = size.cellWidth()
-
-private fun Size.cellWidth() = floor((width - CELL_SPACING) / (CELL_SIZE + CELL_SPACING)).toInt()
-
-private fun Size.cellHeight() = floor((height - CELL_SPACING) / (CELL_SIZE + CELL_SPACING)).toInt()
-
-private fun DrawScope.drawMaze(cells: List<Int>, fills: List<Color?>) {
-    for (i in cells.indices) {
-        val color = fills[i] ?: continue
-        val x = i % cellWidth()
-        val y = i / cellWidth()
-        val x0 = x * CELL_SIZE + (x + 1) * CELL_SPACING
-        val y0 = y * CELL_SIZE + (y + 1) * CELL_SPACING
-        drawRect(
-            color = color,
-            topLeft = Offset(x0, y0),
-            size = Size(CELL_SIZE, CELL_SIZE),
-        )
-        if (cells[i] and S != 0) {
-            drawRect(
-                color = color,
-                topLeft = Offset(x0, y0 + CELL_SPACING),
-                size = Size(CELL_SIZE, CELL_SPACING),
-            )
-        }
-        if (cells[i] and E != 0) {
-            drawRect(
-                color = color,
-                topLeft = Offset(x0 + CELL_SPACING, y0),
-                size = Size(CELL_SPACING, CELL_SIZE),
-            )
-        }
-    }
 }
 
 private data class Edge(val index: Int, val direction: Int, val priority: Double = Math.random())
